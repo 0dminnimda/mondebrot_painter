9df5005dd5190ab46be608b8e5060b87b4599f1c
0dminnimda <0dminnimda@gmail.com>
2022-04-22 20:14:02 +0300

    set_generator.pyx: genereate the real deal - mandelbrot set

--- 2022/mandelbrot_painter/set_generator.pyx
+++ 2022/mandelbrot_painter/set_generator.pyx
@@ -1,32 +1,40 @@
 import numpy as np
 cimport cython
+# from cython.parallel import prange
 
 ctypedef complex complex_t
 ctypedef complex_t [:, :] complex_mem
+ctypedef int [:, :] mand_set_mem
 
-cdef complex_t function(complex_t z, complex_t c):
+
+cdef inline complex_t function(complex_t z, complex_t c):
     return z**2 + c
+
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-@cython.nonecheck(False)
-cdef complex_mem mem_function(complex_mem z, complex_mem c):
-    cdef Py_ssize_t x_max = z.shape[0]
-    cdef Py_ssize_t y_max = z.shape[1]
+cpdef mand_set_mem compute_set(complex_mem c, int max_iter=100):
+    cdef complex_mem z = np.zeros_like(c)
+    cdef mand_set_mem m_set = np.zeros((c.shape[0], c.shape[1]), dtype=np.int32)
+
     cdef Py_ssize_t x, y
+    cdef int i
+    cdef complex_t res
 
-    for x in range(x_max):
-        for y in range(y_max):
-            z[x, y] = function(z[x, y], c[x, y])
+    for i in range(1, max_iter + 1):
+        for x in range(m_set.shape[0]):
+            for y in range(m_set.shape[1]):
+                if m_set[x, y] == 0:
+                    res = z[x, y] = function(z[x, y], c[x, y])
+                    if res.real**2 + res.imag**2 > 4:
+                        m_set[x, y] = i
 
-    return z
+    return m_set
 
-cdef complex_mem compute_set(complex_mem arr):
-    cdef complex_mem z = np.zeros_like(arr)
-    return mem_function(z, arr)
 
-cpdef complex_mem create_set(complex_t point, int width, int height, int dencity=1, double magnification=1.):
+cpdef complex_mem create_grid(complex_t point, int width, int height,
+                              int dencity=1, double magnification=1.):
     xv, yv = np.meshgrid(
         np.linspace(-width*magnification/2, width*magnification/2, width*dencity),
         np.linspace(-height*magnification/2, height*magnification/2, height*dencity))
-    return compute_set(xv + yv * 1j + point)
+    return xv + yv * 1j + point
