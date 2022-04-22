62d42cd76ae8343cd7ac5c4d6409772b8b3129d1
0dminnimda <0dminnimda@gmail.com>
2022-04-22 20:16:47 +0300

    tests\manual.py: output ascii mandelbrot set!

--- 2022/tests/manual.py
+++ 2022/tests/manual.py
@@ -1,6 +1,14 @@
 import numpy as np
-from mandelbrot_painter.set_generator import create_set
+from mandelbrot_painter.set_generator import create_grid, compute_set
 
-print(np.asarray(create_set(-1+6j, 1, 2, dencity=3, magnification=1.5)))
+
+def arrprint(arr):
+    for item in arr:
+        print(*item)
+    print()
+
+
+arrprint(compute_set(create_grid(-1+6j, 1, 2, dencity=3, magnification=1.5)))
+arrprint(compute_set(create_grid(-0.5+0j, 3, 2, dencity=30), 9))
 
 breakpoint
