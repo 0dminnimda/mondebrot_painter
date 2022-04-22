420e9ffb6bc70774f9a7f11484745c8ed6995181
0dminnimda <0dminnimda@gmail.com>
2022-04-22 21:46:46 +0300

    tests\manual.py: make it render the whole thing!

--- 2022/tests/manual.py
+++ 2022/tests/manual.py
@@ -1,5 +1,7 @@
 import numpy as np
+from colour import Color
 from mandelbrot_painter.set_generator import create_grid, compute_set
+from mandelbrot_painter.painter import make_palette, save_set_as_image
 
 
 def arrprint(arr):
@@ -8,7 +10,17 @@ def arrprint(arr):
     print()
 
 
-arrprint(compute_set(create_grid(-1+6j, 1, 2, dencity=3, magnification=1.5)))
-arrprint(compute_set(create_grid(-0.5+0j, 3, 2, dencity=30), 9))
+# arrprint(compute_set(create_grid(-1+6j, 1, 2, dencity=3, magnification=1.5)))
+# arrprint(compute_set(create_grid(-0.5+0j, 3, 2, dencity=30), 9))
+
+max_iter = 50
+
+set = compute_set(create_grid(-0.75+0j, 3, 2, dencity=1000), max_iter)
+
+palette = make_palette(max_iter + 1, Color("black"), Color("green"))
+# print(palette, len(palette))
+
+save_set_as_image(set, palette, "test_img.png")
 
 breakpoint
+
