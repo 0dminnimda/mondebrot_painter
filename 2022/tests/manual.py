aabb22c44c94616dd9ad8ba8606229ade106e695
0dminnimda <0dminnimda@gmail.com>
2023-07-05 20:21:41 +0300

    2022\tests\manual.py: commit what was left

--- 2022/tests/manual.py
+++ 2022/tests/manual.py
@@ -31,10 +31,10 @@ max_iter = 80
 grid = create_grid(
     -0.170337168373994-1.065060284305098j,
     width=13, height=10,
-    dencity=100, magnification=1/500000000000000)
+    dencity=100, magnification=1/50000)
 
-palette = make_palette(max_iter, Color("black"), Color("white"))
+palette = make_palette(max_iter, Color("black"), Color("red"), Color("white"))
 image = paint_the_set(compute_set(grid, max_iter), palette)
 
-name = "test_depth"
+name = "lightnings"
 imwrite(f"images/{name}.png", image)
