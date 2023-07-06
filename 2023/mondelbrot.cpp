031be37fdbf2775dc6c3f0e20952907a6e0e725f
0dminnimda <0dminnimda@gmail.com>
2023-07-06 13:10:21 +0300

    2023\mondelbrot.cpp: extract relevant code into structs

--- 2023/mondelbrot.cpp
+++ 2023/mondelbrot.cpp
@@ -29,6 +29,52 @@ bool out_of_bounds_circle(const complex_t &c) {
 
 using boundary_func_t = std::function<bool(const complex_t &)>;
 
+struct Points {
+    complex_t points[N * N];
+
+    complex_t &operator[](std::size_t i) { return points[i]; }
+
+    complex_t &at(std::size_t i, std::size_t j) { return points[i * N + j]; }
+
+    Points(complex_t center, complex_t direction1, complex_t direction2) {
+        complex_t diagonal = direction1 + direction2;
+        complex_t helf_diagonal = diagonal / 2;
+        complex_t upper_left_corner = center - helf_diagonal;
+
+        for (std::size_t i = 0; i < N; ++i) {
+            for (std::size_t j = 0; j < N; ++j) {
+                at(i, j) = upper_left_corner + (direction1 * i / (N - 1)) +
+                           (direction2 * j / (N - 1));
+            }
+        }
+    }
+};
+
+struct Iterations {
+    std::size_t iterations[N * N];
+
+    std::size_t &operator[](std::size_t i) { return iterations[i]; }
+
+    std::size_t &at(std::size_t i, std::size_t j) { return iterations[i * N + j]; }
+
+    void print(std::size_t max_iter) {
+        for (std::size_t i = 0; i < N; ++i) {
+            for (std::size_t j = 0; j < N; ++j) {
+                std::size_t iter = iterations[i * N + j];
+                if (iter != max_iter) {
+                    // nither max_iter or pallet.size() should never be reached
+                    std::size_t frac_index =
+                        (part_t)iter / (max_iter - 1) * (pallet.size() - 1);
+                    std::cout << pallet[frac_index] << pallet[frac_index];
+                } else {
+                    std::cout << set_color << set_color;
+                }
+            }
+            std::cout << std::endl;
+        }
+    }
+};
+
 struct Mondelbrot {
 public:
     std::size_t max_iter = 100;
@@ -43,6 +89,12 @@ public:
         }
         return max_iter;
     }
+
+    void calculate(Iterations &iterations, Points &points) const {
+        for (std::size_t i = 0; i < N * N; ++i) {
+            iterations[i] = calculate_at(points[i]);
+        }
+    }
 };
 
 int main() {
@@ -50,43 +102,14 @@ int main() {
 
     part_t width = abs_bound ? 3.3 : 2.5;
     complex_t center = complex_t(abs_bound ? -0.5 : -0.75, 0);
-
-    complex_t side_1 = complex_t(width, 0);
-    complex_t side_2 = complex_t(0, width);
-
-    complex_t diagonal = side_1 + side_2;
-    complex_t helf_diagonal = diagonal / 2;
-    complex_t upper_left_corner = center - helf_diagonal;
-
-    complex_t c[N][N];
-    for (std::size_t i = 0; i < N; ++i) {
-        for (std::size_t j = 0; j < N; ++j) {
-            c[i][j] = upper_left_corner + (side_1 * i / (N - 1)) + (side_2 * j / (N - 1));
-        }
-    }
+    Points points(center, complex_t(width, 0), complex_t(0, width));
 
     const Mondelbrot mondelbrot = {
         .max_iter = 10, .boundary = abs_bound ? out_of_bounds_abs : out_of_bounds_circle};
-    std::size_t iterations[N][N];
-    for (std::size_t i = 0; i < N; ++i) {
-        for (std::size_t j = 0; j < N; ++j) {
-            iterations[i][j] = mondelbrot.calculate_at(c[i][j]);
-        }
-    }
 
-    for (std::size_t i = 0; i < N; ++i) {
-        for (std::size_t j = 0; j < N; ++j) {
-            std::size_t iter = iterations[i][j];
-            if (iter != mondelbrot.max_iter) {
-                // nither mondelbrot.max_iter or pallet.size() should never be reached
-                std::size_t frac_index = (part_t)iter / (mondelbrot.max_iter - 1) * (pallet.size() - 1);
-                std::cout << pallet[frac_index] << pallet[frac_index];
-            } else {
-                std::cout << set_color << set_color;
-            }
-        }
-        std::cout << std::endl;
-    }
+    Iterations iterations;
+    mondelbrot.calculate(iterations, points);
+    iterations.print(mondelbrot.max_iter);
 
     return 0;
 }
