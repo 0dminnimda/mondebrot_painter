e48686220932d10896f29eac9a20836e74033688
0dminnimda <0dminnimda@gmail.com>
2025-06-12 22:52:42 +0300

    2025: nob.c strip prefixes

--- 2025/nob.c
+++ 2025/nob.c
@@ -1,19 +1,20 @@
 #define NOB_IMPLEMENTATION
+#define NOB_STRIP_PREFIX
 #include "nob.h"
 
 int main(int argc, char **argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
-    Nob_Cmd cmd = {0};
+    Cmd cmd = {0};
 
-    nob_cmd_append(&cmd, "clang++");
+    cmd_append(&cmd, "clang++");
     nob_cc_flags(&cmd);
     nob_cc_output(&cmd, "mondex.exe");
 
-    nob_cmd_append(&cmd, "main.cpp");
-    nob_cmd_append(&cmd, "-lraylib");
+    cmd_append(&cmd, "main.cpp");
+    cmd_append(&cmd, "-lraylib");
 
-    nob_cmd_append(&cmd, "-O2", "-Wno-unused-const-variable");
+    cmd_append(&cmd, "-O2", "-Wno-unused-const-variable");
 
-    if (!nob_cmd_run_sync(cmd)) return 1;
+    if (!cmd_run_sync(cmd)) return 1;
     return 0;
 }
